f3c1c7de8ee596fdc69adf68a79b53ac658a5540
Marcelo Amorim <marcelo.med.amorim@gmail.com>
2020-07-20 21:31:42 -0300

    Adiciona Home e Post controllers

---
+++ Blog/Blog/Controllers/PostController.cs
@@ -0,0 +1,46 @@
+﻿using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Index()
+        {
+			var listaDePosts = new List<Post>()
+			{
+
+				new Post()
+				{   
+					Titulo = "Harry	Potter1",
+					Resumo =  "Pedra	Filosofal",
+					Categoria  =  "Filme,  Livro" 
+				},
+
+				new Post()
+				{
+					Titulo = "Harry	Potter2",
+					Resumo =  "Pedra	Filosofal",
+					Categoria  =  "Filme,  Livro"
+				},
+
+				new Post()
+				{
+					Titulo = "Harry	Potter3",
+					Resumo =  "Pedra	Filosofal",
+					Categoria  =  "Filme,  Livro"
+				}
+
+			};
+
+			return View(listaDePosts);
+
+		}
+
+	}
+}
