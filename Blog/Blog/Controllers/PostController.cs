04a567657a892ded3f7c83755eec31be55ff1e7e
Marcelo Amorim <marcelo.med.amorim@gmail.com>
2020-07-21 23:27:28 -0300

    Adiciona conexão com banco de dados

--- Blog/Blog/Controllers/PostController.cs
+++ Blog/Blog/Controllers/PostController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,34 +17,75 @@ namespace Blog.Controllers
 		public PostController()
         {
             listaDePosts = new List<Post>()
-			{
-				new Post()
-				{
-					Titulo = "Harry	Potter1",
-					Resumo =  "Pedra	Filosofal",
-					Categoria  =  "Filme,  Livro"
-				},
+			{				
+				new Post(){Titulo = "Arquitetura Moderna e Bauhaus", Resumo =  "Brasília", Categoria  =  "Arquitetura"},
 
-				new Post()
-				{
-					Titulo = "Harry	Potter2",
-					Resumo =  "Pedra	Filosofal",
-					Categoria  =  "Filme,  Livro"
-				},
+				new Post(){Titulo = "Arquitetura Moderna e Bauhaus", Resumo =  "Brasília", Categoria  =  "Arquitetura"},
 
-				new Post()
-				{
-					Titulo = "Harry	Potter3",
-					Resumo =  "Pedra	Filosofal",
-					Categoria  =  "Filme,  Livro"
-				}
+				new Post(){Titulo = "Arquitetura Moderna e Bauhaus", Resumo =  "Brasília", Categoria  =  "Arquitetura"},
+
+				new Post(){Titulo = "Arquitetura Moderna e Bauhaus", Resumo =  "Brasília", Categoria  =  "Arquitetura"},
+
+				new Post(){Titulo = "Arquitetura Moderna e Bauhaus", Resumo =  "Brasília", Categoria  =  "Arquitetura"},
+
+				new Post(){Titulo = "Arquitetura Moderna e Bauhaus", Resumo =  "Brasília", Categoria  =  "Arquitetura"},
+
+				new Post(){Titulo = "Arquitetura Moderna e Bauhaus", Resumo =  "Brasília", Categoria  =  "Arquitetura"},
+
+				new Post(){Titulo = "Arquitetura Moderna e Bauhaus", Resumo =  "Brasília", Categoria  =  "Arquitetura"},
+
+				new Post(){Titulo = "Arquitetura Moderna e Bauhaus", Resumo =  "Brasília", Categoria  =  "Arquitetura"},
+
+				new Post(){Titulo = "Arquitetura Moderna e Bauhaus", Resumo =  "Brasília", Categoria  =  "Arquitetura"},
+
+				new Post(){Titulo = "Arquitetura Moderna e Bauhaus", Resumo =  "Brasília", Categoria  =  "Arquitetura"},
+
+				new Post(){Titulo = "Arquitetura Moderna e Bauhaus", Resumo =  "Brasília", Categoria  =  "Arquitetura"},
+
+				new Post(){Titulo = "Arquitetura Moderna e Bauhaus", Resumo =  "Brasília", Categoria  =  "Arquitetura"},
+
+				new Post(){Titulo = "Arquitetura Moderna e Bauhaus", Resumo =  "Brasília", Categoria  =  "Arquitetura"},
+
+				new Post(){Titulo = "Arquitetura Moderna e Bauhaus", Resumo =  "Brasília", Categoria  =  "Arquitetura"},
+
+				new Post(){Titulo = "Arquitetura Moderna e Bauhaus", Resumo =  "Brasília", Categoria  =  "Arquitetura"},
+
+				new Post(){Titulo = "Arquitetura Moderna e Bauhaus", Resumo =  "Brasília", Categoria  =  "Arquitetura"},
+				
 			};
-
 		}
 
         public IActionResult Index()
-        {			
+        {
+
+			//Como obter dados de post do banco?
+
+			//1 - conection string			
+			//2 - criar objeto de conexão
+			//3 - abrir a conexão
+			//4 - executar o comando = "select * from Post"
+			//5 - jogar o DataTable em uma variável
+			//6 - fechar a conexão
+			//7 - retorna o objeto para o controller
+			//8 - retorna para View
+
+			//var stringConnection = "string de conexão";
+
+			var conexao = new SqlConnection();
+			conexao.Open();
+
+			var select = "select * from Post";
+			var comando = conexao.CreateCommand();
+
+			comando.CommandText = select;
+			var dataReader = comando.ExecuteReader();
+
+			conexao.Close();
+
+			//mapear as colunas para o objeto Post;
+
 			return View(listaDePosts);
+
 		}
 
 		public IActionResult Novo()
